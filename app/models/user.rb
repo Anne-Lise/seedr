909a92060e1b2a9c1d77df9e06372b3ad96cc78b
annelise coudry <alcoudry@gmail.com>
2020-01-21 10:42:41 +0100

    devise authentification en comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   has_many :seedpacks, dependent: :destroy
   has_many :orders, dependent: :destroy
 
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  validates :name, presence: true
-  validates :city, presence: true
+  # devise :database_authenticatable, :registerable,
+  # #        :recoverable, :rememberable, :validatable
+  # validates :name, presence: true
+  # validates :city, presence: true
 end
